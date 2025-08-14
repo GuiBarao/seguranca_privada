aa264b4d4852ed1f004000e2fe558cb91b36b804
GuiBarao <guibarao04@gmail.com>
2025-08-14 16:12:12 -0400

    commit inicial

---
+++ src/test/java/com/guibarao/seguranca_privada/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guibarao.seguranca_privada.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
